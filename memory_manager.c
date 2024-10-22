56ee7c0bf6207b1a94ce0d2563a33d23d3b1749a
sophie cook <Sophiecook0@gmail.com>
2024-10-22 19:38:42 +0200

    test3

--- memory_manager.c
+++ memory_manager.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#//include <stdint.h> 
 
 // structure representing a block of memory within the pool.
 //each block knows its starting address, size, and the next block in the list.
